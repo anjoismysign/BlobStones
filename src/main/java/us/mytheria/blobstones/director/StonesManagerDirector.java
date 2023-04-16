ea9a895c481ba75b225ef7b4ce6819b442812a9a
lbenav8095 <lubensaico@gmail.com>
2023-04-16 03:09:18 -0600

    Added /blobstones command

--- src/main/java/us/mytheria/blobstones/director/StonesManagerDirector.java
+++ src/main/java/us/mytheria/blobstones/director/StonesManagerDirector.java
@@ -1,5 +1,10 @@
 package us.mytheria.blobstones.director;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import us.mytheria.bloblib.managers.ManagerDirector;
 import us.mytheria.blobstones.BlobStones;
 
@@ -15,6 +20,17 @@ public class StonesManagerDirector extends ManagerDirector {
         addManager("Config", new ConfigManager(this));
         addManager("Inventory", new InventoryManager(this));
         addManager("Listener", new ListenerManager(this));
+
+        getPlugin().getCommand("blobstones").setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+                if (!(commandSender instanceof Player))
+                    return false;
+                Player player = (Player) commandSender;
+                getInventoryManager().openWorldNavigator(player);
+                return true;
+            }
+        });
     }
 
     @Override
